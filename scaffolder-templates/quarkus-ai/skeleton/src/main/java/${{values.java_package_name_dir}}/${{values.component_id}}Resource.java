871df7c4168074475dbca3c9a2f52b73cc5b803f
Sebastien LALLEMAND <sld@redhat.com>
2024-08-29 16:29:10 +0200

    add quarkus ai template

---
+++ scaffolder-templates/quarkus-ai/skeleton/src/main/java/${{values.java_package_name_dir}}/${{values.component_id}}Resource.java
@@ -0,0 +1,9 @@
+package ${{values.java_package_name}};
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("${{values.api_endpoint}}")
+public class ${{values.component_id}}Resource {
+
+}
