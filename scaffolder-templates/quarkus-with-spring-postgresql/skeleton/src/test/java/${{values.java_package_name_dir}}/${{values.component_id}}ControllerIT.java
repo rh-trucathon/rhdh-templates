72ad60d111d6f7aed067525dcb581b4d8b28c156
Sebastien LALLEMAND <sld@redhat.com>
2024-09-02 09:20:38 +0200

    change skeleton (full code available)

--- scaffolder-templates/quarkus-with-spring-postgresql/skeleton/src/test/java/${{values.java_package_name_dir}}/${{values.component_id}}ControllerIT.java
+++
@@ -1,8 +0,0 @@
-package ${{values.java_package_name}};
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ${{values.component_id}}ControllerIT extends ${{values.component_id}}ControllerTest {
-    // Execute the same tests but in packaged mode.
-}
