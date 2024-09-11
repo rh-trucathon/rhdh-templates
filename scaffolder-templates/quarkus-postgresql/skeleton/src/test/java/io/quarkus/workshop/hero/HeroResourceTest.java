bb419292af3fccc0252de63418079c6e6809dcff
Sebastien LALLEMAND <sld@redhat.com>
2024-09-11 14:22:41 +0200

    update HeroResourceTest.java starting file

--- scaffolder-templates/quarkus-postgresql/skeleton/src/test/java/io/quarkus/workshop/hero/HeroResourceTest.java
+++ scaffolder-templates/quarkus-postgresql/skeleton/src/test/java/io/quarkus/workshop/hero/HeroResourceTest.java
@@ -1,20 +1,42 @@
 package io.quarkus.workshop.hero;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.List;
+import java.util.Random;
+
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
+import static jakarta.ws.rs.core.Response.Status.CREATED;
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+import static jakarta.ws.rs.core.Response.Status.NO_CONTENT;
+import static jakarta.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
-public class HeroResourceTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class HeroResourceTest {
+
     @Test
-    void testHelloEndpoint() {
+    public void testHelloEndpoint() {
         given()
-        .when().get("${{values.api_endpoint}}")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+                .when().get("${{values.api_endpoint}}/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from Quarkus REST"));
     }
 
 }
