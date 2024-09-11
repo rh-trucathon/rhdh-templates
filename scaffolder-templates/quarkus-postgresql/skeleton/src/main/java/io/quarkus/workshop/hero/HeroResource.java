db29df90d05c715bc6fed932b4c5d22bc5bd2b76
Sebastien LALLEMAND <sld@redhat.com>
2024-09-11 14:15:53 +0200

    update HeroResource.java starting file

--- scaffolder-templates/quarkus-postgresql/skeleton/src/main/java/io/quarkus/workshop/hero/HeroResource.java
+++ scaffolder-templates/quarkus-postgresql/skeleton/src/main/java/io/quarkus/workshop/hero/HeroResource.java
@@ -1,16 +1,38 @@
 package io.quarkus.workshop.hero;
 
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.jboss.resteasy.reactive.RestResponse;
+
+
+
+import java.net.URI;
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("${{values.api_endpoint}}")
+@RunOnVirtualThread
 public class HeroResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
 }
